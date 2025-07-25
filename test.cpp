e16db2faff8260e72f455a301e732a3462786089
kalninsos <145606492+kalninsos@users.noreply.github.com>
2025-07-25 14:30:55 -0500

    rename printscore to printroll, edited calculatescore

--- test.cpp
+++ test.cpp
@@ -12,21 +12,41 @@ vector<int> Roll(int dice_amount) {
 }
 
 int CalculateScore(vector<int> rolls) {
-    int score;
+    int score = 0;
+    int straight16 = 0;
     vector<int> number_frequency(6); // should always be 6 dice
 
+    //build frequency vector
     for(int i = 0; i < 6; i++) {
         number_frequency[rolls[i] - 1]++; // -1 as vectors are 0 indexed (0-5) but our dice rolls are 1-6
     }
 
+    //print frequency vector for debugging
     for(int i = 0; i < 6; i++) {
         cout << number_frequency[i];
+    }
+    cout << endl;
+
+    for(int i = 0; i < 6; i++) {
+        if(number_frequency[i] == 1) {
+            straight16++; //1-6 straight check
+            if(i == 1) {
+                score += 100;
+            }
+            else if(i == 5) {
+                score += 50;
+            }
+        }
+    }
+
+    if(straight16 == 6) {
+        score = 1500;
     }
 
     return score;
 }
 
-void PrintScore(vector<int> rolls) {
+void PrintRolls(vector<int> rolls) {
     for(int i = 0; i < 6; i++) {
             cout << rolls[i];
     }
@@ -42,9 +62,8 @@ int main () {
         cout << "rolling dice!" << endl;
 
         vector<int> rolls = Roll(6);
-        PrintScore(rolls);
+        PrintRolls(rolls);
         int score = CalculateScore(rolls);
-
     }
     else {
         cout << "invalid char" << endl;
